d75ab151e0c5882f71f1dc410323a0e4f72ef08d
Fawaz Butt <fawazbutt@Fawazs-MacBook-Pro-2.local>
2024-02-08 09:53:30 -0500

    added localStorage for Name and Budget values

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const btn = document.querySelector(".btn");
   const container = document.querySelector(".container");
   const budgetMain = document.querySelector(".budget-main");
+  const nameValue = document.getElementById("name").value;
+  const budgetValue = document.getElementById("budget").value;
 
   btn.addEventListener("click", function (e) {
     e.preventDefault();
     container.style.display = "none";
     budgetMain.style.display = "flex";
   });
+  // Name and budget goes into local storage, and the "display none"
+  localStorage.setItem("name", nameValue);
+  localStorage.setItem("budget", budgetValue);
 });
