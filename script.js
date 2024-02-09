399bf61e16ceafb552f93d6056b6c665b024d084
Jamie Yang <yangjm1287@gmail.com>
2024-02-09 16:19:47 -0500

    commits

--- script.js
+++ script.js
@@ -101,62 +101,6 @@ function updateTotals() {
   remainingBalance.innerText = budgetInput.value - startCost;
 }
 
-// function addRow() {
-//   // Get input values
-//   let item = itemInput.value;
-//   let category = categoryInput.value;
-//   let cost = costInput.value;
-
-//   // Get table body
-//   let table = mainTable.getElementsByTagName("tbody")[0];
-
-//   // Create a new row
-//   let newRow = table.insertRow();
-
-//   // Insert cells into the row
-//   let itemCol = newRow.insertCell(0);
-//   let categoryCol = newRow.insertCell(1);
-//   let costCol = newRow.insertCell(2);
-
-//   // Add values to the cells
-//   itemCol.innerHTML = item;
-//   categoryCol.innerHTML = category;
-//   costCol.innerHTML = `$${cost}`;
-
-//   // //set class names to values 
-//   console.log(categoryCol);
-//   let createdCatCol = document.getElementsByTagName("tbody"); 
-//   createdCatCol[0].className = category;
-//   console.log(createdCatCol);
-//   // createdCatCol.classList.add(category);
-
-//   // Clear input fields after adding row
-//   itemInput.value = "";
-//   categoryInput.value = "";
-//   costInput.value = "";
-
-//   // Update remaining budget each time new item is entered
-//   let remainingBalance = document.getElementById("remaining-balance-num");
-
-//   container.style.display = "none";
-//   expenseForm.style.display = "none";
-//   budgetMain.style.display = "flex";
-
-//   let spanStartCost = document.getElementById("spanStartCost");
-//   let startCost = 0;
-
-//   // Total cost updates
-//   for (var i = 1; i < table.rows.length; i++) {
-//     // Get the cell value of the third column (index 2) and parse it as a float
-//     cost = parseFloat(table.rows[i].cells[2].textContent.replace("$", ""));
-
-//     // Add up total cost
-//     startCost += cost;
-//   }
-//   spanStartCost.innerText = `$${startCost}`;
-//   remainingBalance.innerText =budgetInput.value - startCost;
-// }
-
 btn.addEventListener("click", function (e) {
   e.preventDefault();
   //on button click "display: none"
@@ -194,16 +138,14 @@ addPurchaseBtn.addEventListener("click", (e) => {
   addToTable();
   updateTotals()
 
-   //update category breakdown 
-   categoryBreakdown(expenseArray);
-  
+  //update category breakdown 
+  categoryBreakdown(expenseArray);
+
   // clear inputs of form
   itemInput.value = "";
   categoryInput.value = "";
   costInput.value = ""
 
-  //update category breakdown 
-  categoryBreakdown(expenseArray);
   //change display settings
   container.style.display = "none";
   expenseForm.style.display = "none";
