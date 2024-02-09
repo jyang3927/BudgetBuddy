de5de7578239b68ed8056d1b4e0e0c8d15325142
Jamie Yang <yangjm1287@gmail.com>
2024-02-09 13:08:33 -0500

    reorganizing the js and editing some html/css

--- script.js
+++ script.js
@@ -8,27 +8,21 @@ const remainingBalance = document.getElementById("remaining-balance-num");
 const addExpense = document.getElementById("submit-expense-btn"); 
 const expenseForm = document.getElementById("expense-container");
 const addPurchaseBtn = document.getElementById("expense-add-btn"); 
+let itemInput = document.getElementById("itemInput");
+let categoryInput = document.getElementById("categoryInput");
+let costInput = document.getElementById("costInput");
+const mainTable = document.getElementById("mainTable");
 
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  //on button click "display: none"
-  container.style.display = "none";
-  budgetMain.style.display = "flex";
-  // Name and budget goes into local storage
-  localStorage.setItem("name", nameInput.value);
-  localStorage.setItem("budget", budgetInput.value);
-  remainingBalance.innerText = budgetInput.value;
-  console.dir(remainingBalance)
-});
+// FUNCTIONS 
 
 function addRow() {
   // Get input values
-  let item = document.getElementById("itemInput").value;
-  let category = document.getElementById("categoryInput").value;
-  let cost = document.getElementById("costInput").value;
+  let item = itemInput.value;
+  let category = categoryInput.value;
+  let cost = costInput.value;
 
   // Get table body
-  let table = document.getElementById("mainTable").getElementsByTagName("tbody")[0];
+  let table = mainTable.getElementsByTagName("tbody")[0];
 
   // Create a new row
   let newRow = table.insertRow();
@@ -43,10 +37,17 @@ function addRow() {
   categoryCol.innerHTML = category;
   costCol.innerHTML = `$${cost}`;
 
+  // //set class names to values 
+  console.log(categoryCol);
+  let createdCatCol = document.getElementsByTagName("tbody"); 
+  createdCatCol[0].className = category;
+  console.log(createdCatCol);
+  // createdCatCol.classList.add(category);
+
   // Clear input fields after adding row
-  document.getElementById("itemInput").value = "";
-  document.getElementById("categoryInput").value = "";
-  document.getElementById("costInput").value = "";
+  itemInput.value = "";
+  categoryInput.value = "";
+  costInput.value = "";
 
   // Update remaining budget each time new item is entered
   let remainingBalance = document.getElementById("remaining-balance-num");
@@ -55,22 +56,32 @@ function addRow() {
   expenseForm.style.display = "none";
   budgetMain.style.display = "flex";
 
-  var spanStartCost = document.getElementById("spanStartCost");
-  var startCost = 0
+  let spanStartCost = document.getElementById("spanStartCost");
+  let startCost = 0;
 
-// Total cost updates
-for (var i = 1; i < table.rows.length; i++) {
-  // Get the cell value of the third column (index 2) and parse it as a float
-   cost = parseFloat(table.rows[i].cells[2].textContent.replace("$", ""));
+  // Total cost updates
+  for (var i = 1; i < table.rows.length; i++) {
+    // Get the cell value of the third column (index 2) and parse it as a float
+    cost = parseFloat(table.rows[i].cells[2].textContent.replace("$", ""));
 
-  // Add up total cost
-  startCost += cost;
+    // Add up total cost
+    startCost += cost;
+  }
+  spanStartCost.innerText = `$${startCost}`;
+  remainingBalance.innerText =budgetInput.value - startCost;
 }
-spanStartCost.innerText = `$${startCost}`
-remainingBalance.innerText =budgetInput.value - startCost
 
-}
-
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  //on button click "display: none"
+  container.style.display = "none";
+  budgetMain.style.display = "flex";
+  // Name and budget goes into local storage
+  localStorage.setItem("name", nameInput.value);
+  localStorage.setItem("budget", budgetInput.value);
+  remainingBalance.innerText = budgetInput.value;
+  console.dir(remainingBalance)
+});
 
 //Add expense click will bring form to display
 addExpense.addEventListener("click", function (e) {
@@ -84,6 +95,12 @@ addExpense.addEventListener("click", function (e) {
 //Add Purchase Button click will bring back to main page with updated table
 addPurchaseBtn.addEventListener("click", (e) => {
   e.preventDefault(); 
+  //call function to create new item row 
+  addRow();
+
+  //set class equal to category of purchase 
+
+  //change display settings
   container.style.display = "none";
   expenseForm.style.display = "none";
   budgetMain.style.display = "flex";
